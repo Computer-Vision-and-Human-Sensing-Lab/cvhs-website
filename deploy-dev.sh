b15a7b33e9e622a14a711790729be455698c3704
james-oldfield <jamesalexanderoldfield@gmail.com>
2023-04-11 20:03:20 +0100

    update names

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# publishes the site to the live development link: http://eecs.qmul.ac.uk/~jo001/patras-website/
-bundle exec jekyll build --baseurl /~jo001/patras-website/; rsync -a _site/ frank:~/public_html/patras-website/ --progress
+# publishes the site to the live development link: http://eecs.qmul.ac.uk/~jo001/cvhs-website/
+bundle exec jekyll build --baseurl /~jo001/cvhs-website/; rsync -a _site/ frank:~/public_html/cvhs-website/ --progress
