4a514ee33f45d5b7833980b4d84c6311fa4516a1
Ioannis Patras <i.patras@qmul.ac.uk>
2024-03-18 11:54:59 +0000

    no changes

--- deploy-ioannis.sh
+++ deploy-ioannis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # publishes the site to the live productiong link: http://eecs.qmul.ac.uk/~ioannisp/
-bundle exec jekyll build --baseurl /~ioannisp/; rsync -a _site/ frank.eecs.qmul.ac.uk:~/public_html/ --progress
+bundle exec jekyll build --baseurl /~ioannisp/; rsync -a _site/ ioannisp@frank.eecs.qmul.ac.uk:~/public_html/ --progress
